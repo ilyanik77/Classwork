657fe2d4be39570e95e1417ae8f99fe831784caf
ilyanik77 <ilyakhlamov77@gmail.com>
2022-10-16 12:07:01 +0300

    Добавили task 004.py

--- Seminar_5/task 004.py
+++ Seminar_5/task 004.py
@@ -1,2 +1,14 @@
 # Дан список чисел. Создайте список, в который попадают числа, описываемые возрастающую последовательность. 
 # Порядок элементов менять нельзя.
+
+
+    
+list = [1, 5, 2, 3, 4, 6, 1, 7]
+list_res=[]
+for j in range(1, len(list)):
+    list_temp = [list[0]]
+    for i in range(j, len(list)):
+        if list[i] > list_temp[-1]:
+            list_temp.append(list[i])
+    list_res.append(list_temp)
+print(list, list_res)
